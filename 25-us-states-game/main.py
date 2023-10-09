c381d57156ac268f7b5bc25f38a56fac28d04e84
mkubincova <mkubincova@proton.me>
2023-10-09 11:38:22 +0200

    feat: day 26

--- 25-us-states-game/main.py
+++ 25-us-states-game/main.py
@@ -17,25 +17,24 @@ all_states = data.state.tolist()
 guessed_states = []
 
 
+def print_state(state_data, color="black"):
+    pen.color(color)
+    pen.goto(x=int(state_data.x.iloc[0]), y=int(state_data.y.iloc[0]))
+    pen.write(state_data.state.iloc[0])
+
+
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="Type another state name:").title()
     if answer_state == "Exit":
-        # states_to_learn = []
-        for state in all_states:
-            if state not in guessed_states:
-                state_data = data[data.state == state]
-                pen.color("red")
-                pen.goto(x=int(state_data.x.iloc[0]), y=int(state_data.y.iloc[0]))
-                pen.write(state_data.state.iloc[0])
-        #         states_to_learn.append(state)
-        # new_data = pandas.DataFrame(states_to_learn)
-        # new_data.to_csv("states_to_lear.csv")
+        missing_states = [state for state in all_states if state not in guessed_states]
+        for state in missing_states:
+            print_state(data[data.state == state], "red")
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_lear.csv")
         break
     if answer_state in all_states and answer_state not in guessed_states:
-        state_data = data[data.state == answer_state]
-        pen.goto(x=int(state_data.x.iloc[0]), y=int(state_data.y.iloc[0]))
-        pen.write(answer_state)
+        print_state(data[data.state == answer_state])
         guessed_states.append(answer_state)
 
 

03db0348bdab49161ae2291eed1a2fea065a3c45
mkubincova <mkubincova@proton.me>
2023-11-14 11:08:45 +0100

    feat: day 60

--- 59-blog-capstone-upgrade/main.py
+++ 59-blog-capstone-upgrade/main.py
@@ -1,5 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import os
+from dotenv import load_dotenv
+import smtplib
+
+load_dotenv()
+GMAIL_PSW = os.getenv("GMAIL_PSW")
+GMAIL_EMAIL = os.getenv("GMAIL_EMAIL")
 
 app = Flask(__name__)
 posts = requests.get("https://api.npoint.io/66b20ef4e6ceda5be835").json()
@@ -15,9 +22,25 @@ def about():
     return render_template('about.html')
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template('contact.html')
+    if request.method == 'POST':
+        data = request.form
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(GMAIL_EMAIL, GMAIL_PSW)
+            connection.sendmail(
+                from_addr=GMAIL_EMAIL,
+                to_addrs=GMAIL_EMAIL,
+                msg=f"Subject:Contact form submit:\n\n"
+                    f"Name: {data['name']}\n"
+                    f"Email: {data['email']}\n"
+                    f"Phone: {data['phone']}\n"
+                    f"Message: {data['message']}\n"
+            )
+        return render_template('contact.html', msg_sent=True)
+    else:
+        return render_template('contact.html', msg_sent=False)
 
 
 @app.route("/post/<int:post_id>")
